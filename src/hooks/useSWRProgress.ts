ad97d56deafec1f3389e2caf6fbe0d4ba09e520d
Chris Laplante <chris.laplante@agilent.com>
2024-05-15 22:23:59 -0400

    hooks: useSWRProgress: fix typing errors

--- src/hooks/useSWRProgress.ts
+++ src/hooks/useSWRProgress.ts
@@ -1,12 +1,13 @@
-import {useRef, useState} from "react";
-import axios from "axios";
+import {useState} from "react";
+import axios, {AxiosRequestConfig} from "axios";
 import useSWR from "swr";
 
-export const useSWRProgress = (key, options?) => {
+export const useSWRProgress = (key: string, options?: AxiosRequestConfig<never>) => {
     const [progress, setProgress] = useState<number>(0);
     const [done, setDone] = useState<boolean>(false);
 
-    return [useSWR(key, (url) => axios.get(url, {
+    return [useSWR(key, (url: string) => axios.get(url, {
+            ...options,
             onDownloadProgress: progressEvent => {
                 const percentage = Math.round((progressEvent.loaded * 100) / progressEvent.total);
                 console.log(percentage);
