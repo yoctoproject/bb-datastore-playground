b11c4a9a770ea0b71900b74660fb3cc0270da64a
Chris Laplante <mostthingsweb@gmail.com>
2025-12-14 12:01:45 -0500

    start hooking up status panel

--- src/main/components/statusPanel/index.tsx
+++ src/main/components/statusPanel/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { usePyodideWorker } from "../../hooks/usePyodideWorker";
 
 export const StatusPanel: React.FC = () => {
-    return <div>Status!</div>;
+    const { workerState } = usePyodideWorker();
+
+    return <div>Status! {JSON.stringify(workerState)}</div>;
 };
