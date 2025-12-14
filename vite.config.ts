582360bfac7b6436b1ed83b21082e82b86dc5be5
Chris Laplante <mostthingsweb@gmail.com>
2025-12-14 17:04:26 -0500

    vite: do full reload when editing workers, otherwise things break

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const fullReloadWorkers = () => {
+    const workerDir = path.resolve(process.cwd(), "src/pyodide-worker");
+
+    return {
+        name: "full-reload-pyodide-worker",
+        handleHotUpdate({ file, server }) {
+            if (file.startsWith(workerDir)) {
+                server.ws.send({ type: "full-reload", path: "*" });
+            }
+        },
+    };
+};
+
 export default defineConfig({
     base: "/bb-datastore-playground/",
-    plugins: [react()],
+    plugins: [react(), fullReloadWorkers()],
     build: {
         outDir: "dist",
         sourcemap: true,
