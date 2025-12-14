e7a52dbc2c694542b7edb6e913d5cf3be50e42e6
Chris Laplante <mostthingsweb@gmail.com>
2025-12-14 17:09:14 -0500

    move import hook code from worker to import_hooks.py

---
+++ src/pyodide-worker/import_hooks.py
@@ -0,0 +1,113 @@
+import os.path
+import sys
+from importlib.abc import Loader, MetaPathFinder
+from importlib.util import spec_from_file_location, spec_from_loader
+
+import _frozen_importlib
+import _frozen_importlib_external
+
+
+class MyMetaFinder(MetaPathFinder):
+    def find_spec(self, fullname, path=None, target=None):
+        print(f"importing {fullname}")
+
+        if path is None or path == "":
+            path = sys.path
+        if "." in fullname:
+            *parents, name = fullname.split(".")
+        else:
+            name = fullname
+        for entry in path:
+            if os.path.isdir(os.path.join(entry, name)):
+                # this module has child modules
+                filename = os.path.join(entry, name, "__init__.py")
+                submodule_locations = [os.path.join(entry, name)]
+            else:
+                filename = os.path.join(entry, name + ".py")
+                submodule_locations = None
+            if not os.path.exists(filename):
+                continue
+
+            print(">> " + filename)
+
+            return spec_from_file_location(
+                fullname,
+                filename,
+                loader=MyLoader(filename),
+                submodule_search_locations=submodule_locations,
+            )
+
+        # we don't know how to import this
+        return None
+
+
+class MyLoader(Loader):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def create_module(self, spec):
+        # Use default module creation semantics
+        return None
+
+    def exec_module(self, module):
+        print("FILE NAME: " + self.filename)
+        if self.filename.endswith("fcntl/__init__.py"):
+            data = """
+from unittest.mock import Mock
+fcntl = Mock()
+            """
+        else:
+            with open(self.filename) as f:
+                data = f.read()
+
+        try:
+            exec(data, vars(module))
+        except Exception as e:
+            print(e)
+            raise e
+
+
+class BuiltinImporterShim(_frozen_importlib.BuiltinImporter):
+    @classmethod
+    def find_spec(cls, fullname, path=None, target=None):
+        ret = super().find_spec(fullname, path, target)
+
+        if ret:
+            print(f"shim handling: {ret}")
+        return ret
+
+
+class FrozenImporterShim:
+    _original_importer = None
+
+    @classmethod
+    def set_original_importer(cls, imp):
+        cls._original_importer = imp
+
+    @classmethod
+    def find_spec(cls, fullname, path=None, target=None):
+        if fullname == "fcntl":
+            return spec_from_loader("fcntl", MyLoader("fcntl/__init__.py"))
+        ret = cls._original_importer.find_spec(fullname, path, target)
+
+        if ret:
+            print(f"frozen shim handling: {ret}")
+        return ret
+
+
+print(sys.meta_path)
+js_finder = sys.meta_path.pop()
+path_finder = sys.meta_path.pop()
+frozen_finder = sys.meta_path.pop()
+builtin = sys.meta_path.pop()
+
+#sys.meta_path.append(MyMetaFinder())
+sys.meta_path.append(path_finder)
+
+i = FrozenImporterShim()
+i.set_original_importer(frozen_finder)
+sys.meta_path.append(i)
+
+sys.meta_path.append(BuiltinImporterShim())
+
+print(sys.meta_path)
--- src/pyodide-worker/worker.ts
+++ src/pyodide-worker/worker.ts
@@ -60,6 +60,9 @@ export class MyWorker {
         const helpersPromise = fetch(
             new URL("./my_helpers.py", import.meta.url)
         ).then((res) => res.text());
+        const importHooksPromise = fetch(
+            new URL("./import_hooks.py", import.meta.url)
+        ).then((res) => res.text());
         const bitbakePromise = axios({
             method: "get",
             url: bitbakeUrl,
@@ -79,11 +82,13 @@ export class MyWorker {
             await printAll("done starting!");
         };
 
-        const [bitbakeData, , helpersSource] = await Promise.all([
-            bitbakePromise,
-            pyodidePromise(),
-            helpersPromise,
-        ]);
+        const [bitbakeData, , helpersSource, importHookSource] =
+            await Promise.all([
+                bitbakePromise,
+                pyodidePromise(),
+                helpersPromise,
+                importHooksPromise,
+            ]);
 
         console.log("unpacking...");
         self.pyodide.unpackArchive(bitbakeData, "zip", {
@@ -91,116 +96,18 @@ export class MyWorker {
         });
 
         console.log("applying import hooks...");
-        self.pyodide.runPython(`
-import os.path
+        self.pyodide.FS.writeFile("/import_hooks.py", importHookSource);
+        self.pyodide.runPython(
+            `
+import importlib
 import sys
-from importlib.abc import Loader, MetaPathFinder
-from importlib.util import spec_from_file_location, spec_from_loader
 
-import _frozen_importlib
-import _frozen_importlib_external
-
-
-class MyMetaFinder(MetaPathFinder):
-    def find_spec(self, fullname, path=None, target=None):
-        print(f"importing {fullname}")
-
-        if path is None or path == "":
-            path = sys.path
-        if "." in fullname:
-            *parents, name = fullname.split(".")
-        else:
-            name = fullname
-        for entry in path:
-            if os.path.isdir(os.path.join(entry, name)):
-                # this module has child modules
-                filename = os.path.join(entry, name, "__init__.py")
-                submodule_locations = [os.path.join(entry, name)]
-            else:
-                filename = os.path.join(entry, name + ".py")
-                submodule_locations = None
-            if not os.path.exists(filename):
-                continue
-
-            print(">> " + filename)
-
-            return spec_from_file_location(fullname, filename, loader=MyLoader(filename),
-                                           submodule_search_locations=submodule_locations)
-
-        # we don't know how to import this
-        return None
-
-
-class MyLoader(Loader):
-    def __init__(self, filename):
-        self.filename = filename
-
-    def create_module(self, spec):
-        # Use default module creation semantics
-        return None
-
-    def exec_module(self, module):
-        print("FILE NAME: " + self.filename)
-        if self.filename.endswith("fcntl/__init__.py"):
-            data = """
-from unittest.mock import Mock
-fcntl = Mock()
-            """
-        else:
-            with open(self.filename) as f:
-                data = f.read()
-
-        try:
-            exec(data, vars(module))
-        except Exception as e:
-            print(e)
-            raise e
-
-
-class BuiltinImporterShim(_frozen_importlib.BuiltinImporter):
-    @classmethod
-    def find_spec(cls, fullname, path=None, target=None):
-        ret = super().find_spec(fullname, path, target)
-
-        if ret:
-            print(f"shim handling: {ret}")
-        return ret
-
-
-class FrozenImporterShim:
-    _original_importer = None
-
-    @classmethod
-    def set_original_importer(cls, imp):
-        cls._original_importer = imp
-
-    @classmethod
-    def find_spec(cls, fullname, path=None, target=None):
-        if fullname == "fcntl":
-            return spec_from_loader("fcntl", MyLoader("fcntl/__init__.py"))
-        ret = cls._original_importer.find_spec(fullname, path, target)
-
-        if ret:
-            print(f"frozen shim handling: {ret}")
-        return ret
-
-print(sys.meta_path)
-js_finder = sys.meta_path.pop()
-path_finder = sys.meta_path.pop()
-frozen_finder = sys.meta_path.pop()
-builtin = sys.meta_path.pop()
-
-#sys.meta_path.append(MyMetaFinder())
-sys.meta_path.append(path_finder)
-
-i = FrozenImporterShim()
-i.set_original_importer(frozen_finder)
-sys.meta_path.append(i)
-
-sys.meta_path.append(BuiltinImporterShim())
-
-print(sys.meta_path)
-        `);
+if "/" not in sys.path:
+    sys.path.insert(0, "/")
+importlib.invalidate_caches()
+import import_hooks
+`
+        );
 
         self.pyodide.runPython(
             `
@@ -216,9 +123,6 @@ import bb.siggen
         self.pyodide.runPython(
             `
 import importlib
-import sys
-if "/" not in sys.path:
-    sys.path.insert(0, "/")
 importlib.invalidate_caches()
 from my_helpers import segment_code, parsehelper
 `
